817418e973eccb2ab91e93b232d70fe468968cab
DovYoung <1591759055@qq.com>
2016-08-23 00:33:16 +0800

    添加一个文件夹存放工具

---
+++ DYTools/WeakTimer/DYWeakTimerTargetObj.m
@@ -0,0 +1,36 @@
+//
+//  HMWeakTimerTargetObj.m
+//  08-定时器的坑-(理解)
+//
+//  Created by hm04 on 16/6/10.
+//  Copyright © 2016年 hm04. All rights reserved.
+//
+
+#import "DYWeakTimerTargetObj.h"
+
+@interface DYWeakTimerTargetObj()
+
+@property (nonatomic, weak) id aTarget;
+@property (nonatomic, assign) SEL aSelector;
+@end
+/**
+ *  该类的作用是用来接管定时器的强引用
+ */
+@implementation DYWeakTimerTargetObj
+
++ (NSTimer *)scheduledTimerWithTimeInterval:(NSTimeInterval)ti target:(id)aTarget selector:(SEL)aSelector userInfo:(nullable id)userInfo repeats:(BOOL)yesOrNo {
+    
+    // 创建当前类对象
+    DYWeakTimerTargetObj *obj = [[DYWeakTimerTargetObj alloc] init];
+    obj.aTarget = aTarget; // 控制器
+    obj.aSelector = aSelector; // 控制器的update方法
+    
+    return [NSTimer scheduledTimerWithTimeInterval:ti target:obj selector:@selector(update:) userInfo:userInfo repeats:yesOrNo];
+}
+
+- (void)update:(NSTimer *)timer {
+//    NSLog(@"%s", __FUNCTION__);
+
+    [self.aTarget performSelector:self.aSelector withObject:timer];
+}
+@end
